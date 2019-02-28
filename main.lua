ab66d99739f43c8f70bb855f5be35b224f43d1b3
Dennis Freitag <dennis@Dennis-MBP.local>
2019-02-28 20:23:40 +0100

    graphics added

--- main.lua
+++ main.lua
@@ -8,6 +8,15 @@ function love.load()
   love.window.setTitle('Cookie Clicker')
 end
 
+function love.keypressed(key)
+  if key == 'escape' then
+    love.event.quit()
+  end
+end
+
+function love.mousepressed(x, y, button)
+  
+end
 
 function love.update(dt)
 
