407ee03d05a702f171dcf92bdabd77ee9df28780
d5f6 <dennisfreitag@gmail.com>
2019-03-02 10:53:15 +0100

    cookie clickable now

--- main.lua
+++ main.lua
@@ -1,18 +1,30 @@
 --[[
   Cookie Clicker
 
-  Author: Dennis Freitag
+  Author: d5f6
 ]]
 
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
+local WINDOW_WIDTH = 1280
+local WINDOW_HEIGHT = 720
 
-cookieTexture = love.graphics.newImage('graphics/cookie.png')
+local cookieTexture = love.graphics.newImage('graphics/cookie.png')
+
+local cookies = 0
+
+local largeFont = love.graphics.newFont(32)
+
+local left = WINDOW_WIDTH / 2 - cookieTexture:getWidth() / 2
+local right = left + cookieTexture:getWidth()
+local top = WINDOW_HEIGHT / 2 - cookieTexture:getHeight() / 2 - 64
+local bottom = top + cookieTexture:getHeight()
+
 
 function love.load()
   love.window.setTitle('Cookie Clicker')
 
   love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT)
+
+  love.graphics.setFont(largeFont)
 end
 
 function love.keypressed(key)
@@ -22,19 +34,31 @@ function love.keypressed(key)
 end
 
 function love.mousepressed(x, y, button)
-
+  if button == 1 then
+    cookies = cookies + 1
+  end
 end
 
 function love.update(dt)
-
+  local x, y = love.mouse.getPosition()
+  
+  
 end
 
 
 function love.draw()
+  love.graphics.printf(
+    'Cookies: ' .. tostring(cookies), 
+    0, 
+    16, 
+    WINDOW_WIDTH, 
+    'center'
+  )
+  
   love.graphics.draw(
     cookieTexture, 
-    WINDOW_WIDTH / 2 - cookieTexture:getWidth() / 2, 
-    WINDOW_HEIGHT / 2 - cookieTexture:getHeight() / 2 - 64
+    left, 
+    top
   )
 
 end
